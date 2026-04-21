ebea83491abfd8d27d70ef6035a02f09cca74387
alzaaa6 <marc.alzamora.02@gmail.com>
2026-04-21 21:02:37 +0200

    Closes #6: Netejo el menú principal, elimino el botó de exit i netejo el codi del fitxer main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@ import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 $(function() { 
     $('#play').on('click', function(){
         let aliasJugador = prompt("Introdueix el teu àlies per jugar:");
-        console.log("Àlies del jugdor: " + aliasJugador);
-        alert("Benvingut, " + aliasJugador + "! El joc començarà aviat.");
-        window.location.assign("./html/canvasgame.html");
+        if (aliasJugador) {
+            sessionStorage.setItem('playerAlias', aliasJugador);
+            sessionStorage.removeItem('load'); 
+            window.location.assign("./html/canvasgame.html");
+        }
+    });
+
+    $('#scores').on('click', function(){
+        console.error("Opció no implementada");
     });
 
     $('#options').on('click', function(){
-        console.error("Opció no implementada");
+        window.location.assign("./html/options.html");
     });
 
     $('#saves').on('click', function(){
@@ -18,42 +24,3 @@ $(function() {
     });
 });
 
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        sessionStorage.removeItem('load');
-        window.location.assign("./html/canvasgame.html");
-    });
-
-    document.getElementById('options').addEventListener('click', 
-    function(){
-        window.location.assign("./html/options.html");
-    });
-
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        let to_load = localStorage.save;
-        fetch('../php/load.php', {
-            method: "POST",
-            body: JSON.stringify({}),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(response => response.json())
-        .then(json => to_load = (!json.error)?JSON.stringify(json.save): localStorage.save)
-        .catch (err => {
-            console.error(err);
-            console.warn("La partida s'intentarà carregar de local");
-        });
-
-        if (!to_load) {
-            alert("No hi ha cap partida a carregar");
-            return;
-        }
-        sessionStorage.load = to_load;
-        window.location.assign("./html/canvasgame.html");
-    });
-
-    $('#exit').on('click', function(){
-        console.warn("No es pot sortir!");
-    })
-});
