7597e61cbef21d22bb93abdf4f62bf3886c2df1d
Marc <marc.alzamora.02@gmail.com>
2026-04-22 18:14:05 +0200

    Modificació del menú per tenir dues opcions per els dos modes de joc disponibles

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
 
 $(function() { 
-    $('#play').on('click', function(){
+
+    function iniciarPartida(mode) {
         let aliasJugador = prompt("Introdueix el teu àlies per jugar:");
         if (aliasJugador) {
             sessionStorage.setItem('playerAlias', aliasJugador);
             sessionStorage.removeItem('load'); 
-            window.location.assign("./html/canvasgame.html");
+            window.location.assign("./html/canvasgame.html?mode=" + mode);
         }
+    }
+
+    $('#play1').on('click', function(){
+        iniciarPartida(1);
+    });
+    
+    $('#play2').on('click', function(){
+        iniciarPartida(2);
     });
 
     $('#scores').on('click', function(){
